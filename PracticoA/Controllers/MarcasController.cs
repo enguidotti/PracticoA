b69b992d1eb2048cbe965a1a989abda13213a5ef
Enso <Gurix@DESKTOP-06R4K9S>
2021-11-24 10:58:41 -0300

    Agregar archivos de proyecto.

---
+++ PracticoA/Controllers/MarcasController.cs
@@ -0,0 +1,36 @@
+﻿using PracticoA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticoA.Controllers
+{
+    public class MarcasController : Controller
+    {
+        private practico1Entities db = new practico1Entities(); 
+        // GET: Marcas
+        public ActionResult Index()
+        {
+            return View(db.Marca.ToList().OrderBy(m=>m.nombre));
+        }       
+        public ActionResult ModalMarca()
+        {
+            return PartialView("_ModalMarca");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ModalMarca(Marca marca)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Marca.Add(marca);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return PartialView("_ModalMarca", marca);
+        }
+    }
+}
---
+++ PracticoA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PracticoA.MvcApplication" Language="C#" %>
