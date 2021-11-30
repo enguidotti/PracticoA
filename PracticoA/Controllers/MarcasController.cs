518826f410abe43d88d4f9b4a73b7645870891b4
Enso <Gurix@DESKTOP-06R4K9S>
2021-11-30 12:54:29 -0300

    marcas editar y eliminar

--- PracticoA/Controllers/MarcasController.cs
+++ PracticoA/Controllers/MarcasController.cs
@@ -1,6 +1,7 @@
 ﻿using PracticoA.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,12 +10,12 @@ namespace PracticoA.Controllers
 {
     public class MarcasController : Controller
     {
-        private practico1Entities db = new practico1Entities(); 
+        private practico1Entities db = new practico1Entities();
         // GET: Marcas
         public ActionResult Index()
         {
-            return View(db.Marca.ToList().OrderBy(m=>m.nombre));
-        }       
+            return View(db.Marca.ToList().OrderBy(m => m.nombre));
+        }
         public ActionResult ModalMarca()
         {
             return PartialView("_ModalMarca");
@@ -31,6 +32,32 @@ namespace PracticoA.Controllers
             }
 
             return PartialView("_ModalMarca", marca);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id != null)
+            {
+                Marca marca = db.Marca.Find(id);
+                return PartialView("_Edit", marca);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Edit(Marca marca)
+        {
+            db.Entry(marca).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int? id)
+        {
+            if(id != null)
+            {
+                Marca marca = db.Marca.Find(id);
+                db.Marca.Remove(marca);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
